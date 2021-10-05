ccbf13a1e9c075ba9d7424cdb62f8a12c99acfe1
Gustavo González <sgu_sigu@msn.com>
2021-10-05 15:30:49 -0600

    Add index.js file

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+// let userList = [];
+
+// #region GET
+
+app.get('/', function(req,res){
+	res.send("Hello World!");
+});
+
+/*app.get('/users', function(req, res){
+	res.json(userList);
+});
+
+app.get('/user/:id', function(req,res){
+	// userList.push(0);
+	const user = userList.find(usr => usr.id == req.params.id);
+	console.log(userList);
+	res.json(user);
+});*/
+
+// #endregion GET
+
+// #region POST
+
+/*app.post('/user', function(req,res){
+	userList.push({id: 0});
+
+	res.json(userList);
+})*/
+
+// #endregion POST
+
+app.listen(3000, function(){
+	console.log("Server running");
+});
